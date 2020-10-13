49c09b0a7abb3da874d4787d88a46ea421f4b108
hamsoace <hamsoace@gmail.com>
2020-10-13 22:57:22 +0300

    Buffered Reader

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
-public class App {
-    public static void main(String[] args){
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class App {
+    public static BufferedReader main(String[] args){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return bufferedReader;
     }
 }
