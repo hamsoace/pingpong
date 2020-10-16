c4afc2e47e0c413d6cd83c92c4386d2f38224ed1
hamsoace <hamsoace@gmail.com>
2020-10-16 12:49:44 +0300

    Tests working

--- src/App.java
+++ src/App.java
@@ -1,9 +1,24 @@
+import models.Pingpong;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class App {
-    public static BufferedReader main(String[] args){
+    public static void main(String[] args){
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        return bufferedReader;
+        System.out.println("Welcome to the Ping Pong game!");
+        System.out.println("Enter any number: ");
+        try{
+            String usersNumber = bufferedReader.readLine();
+            int intUserNumber = Integer.parseInt(usersNumber);
+            Pingpong pingpong = new Pingpong();
+            ArrayList<Object>pingpongResult = pingpong.playPingPong(intUserNumber);
+            System.out.println(pingpongResult);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
